aa3c8203ac618db73fd6f9d021aa45586fdb211a
oshan pandit <oshanpandit@gmail.com>
2022-01-02 16:04:08 +0530

    ejs used

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express=require('express');
 
+const path=require('path');
+
 const app=express();
 
 const port=8000;
+
+app.set('view engine','ejs');
+
+app.set('views',path.join(__dirname,'views'));
 
 
 app.get('/',function(req,res){
